241956bd6b7178552466b253c5cccbdbf38131bd
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-06 12:05:57 -0300

    :wrench: chore: Creates the services for the sign up and sign in routes

--- src/controllers/authRoutes.ts
+++ src/controllers/authRoutes.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from "express";
+
 import * as authInterface from "../interfaces/authInterfaces";
+import * as authService from "../services/authServices";
 
 async function signIn(req: Request, res: Response) {
     const { email, password } : authInterface.IAuthBodyData = req.body;
+    
+    await authService.login(email, password);
     
     res.sendStatus(200);
 }
 
 async function signUp(req: Request, res: Response) {
     const { email, password } : authInterface.IAuthBodyData = req.body;
+
+    await authService.createUser(email, password);
 
     res.sendStatus(201);
 }
---
+++ src/services/authServices.ts
@@ -0,0 +1,11 @@
+
+
+async function login(email: string, password: string) {
+
+}
+
+async function createUser(email: string, password: string) {
+
+}
+
+export { createUser, login };
