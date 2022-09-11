b9a0bf6f82a48cb2e5bc5f728e45afd5c75fba96
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-11 19:11:33 -0300

    :wrench: chore: Created the notes repository

---
+++ src/repositories/notesRepository.ts
@@ -0,0 +1,18 @@
+import { database } from "../config/postegres";
+
+export async function findMoreThanOneTitle(userId: number, title: string) {
+}
+
+async function createNote() {
+
+}
+
+export async function getUserNotes(userId: number) {
+}
+
+export async function getNoteById(userId: number, noteId: number) {
+
+}
+
+export async function deleteNote(noteId: number) {
+}
