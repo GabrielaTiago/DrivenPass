943f866498a60aa9d2d0e378ba42cf0214b0a334
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-11 18:31:45 -0300

    :wrench: chore: Created the notes schema

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,10 +1,12 @@
 import { ISchemas } from "../interfaces/schemasInterface";
 import { authSchema } from "./authSchema";
 import { credentialSchema } from "./credentialSchema";
+import { noteSchema } from "./noteSchema";
 
 const schemas: ISchemas = {
     "auth": authSchema,
-    "credential": credentialSchema
+    "credential": credentialSchema,
+    "note": noteSchema
 };
 
 export { schemas };
