c0c8c0f6a82e324a160b5d0c3d5b359ae0bec4f1
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-11 19:07:07 -0300

    :wrench: chore: Created the notes services

---
+++ src/services/notesServices.ts
@@ -0,0 +1,12 @@
+
+export async function createNote() {
+}
+
+export async function getUserNotes(userId: number) {
+}
+
+export async function getNoteById(userId: number, noteId: number) {
+}
+
+export async function deleteNote(userId: number, noteId: number) {
+}
