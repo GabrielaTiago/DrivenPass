c46c670a81c72df73089242b9c05da2c5f9a330e
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-12 07:55:32 -0300

    :wrench: chore: Creates the card services

---
+++ src/services/wifiSercices.ts
@@ -0,0 +1,12 @@
+
+export async function createWifi() {
+}
+
+export async function getUserWifis(userId: number) {
+}
+
+export async function getWifiById(userId: number, wifiId: number) {
+}
+
+export async function deleteWifi(userId: number, wifiId: number) {
+}
