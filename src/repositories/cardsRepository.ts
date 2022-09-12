f29fb73ecd41dd930aadaba675e6ea13a93b7a86
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-11 22:51:27 -0300

    :wrench: chore: Creates the card repository

---
+++ src/repositories/cardsRepository.ts
@@ -0,0 +1,16 @@
+import { database } from "../config/postegres";
+
+export async function findMoreThanOneNickname(userId: number, nickname: string) {
+}
+
+export async function createCard() {
+}
+
+export async function getUserCards(userId: number) {
+}
+
+export async function getCardById(userId: number, cardId: number) {
+}
+
+export async function deleteCard(cardId: number) {
+}
