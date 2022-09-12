46931af09572da74ed786ef2f448ab59a8b2058d
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-11 22:29:44 -0300

    :boom: fix: Changes in the schema

--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const cardSchema = joi.object({
     nickname: joi.string().max(20).trim().required(),
-    printedName: joi.string().trim().max(50).required(),
+    printedName: joi.string().trim().max(50).uppercase().regex(/^[A-Z\s]+$/).required(),
     number: joi.string().trim().min(16).max(16).regex(/^[0-9]+$/).required(),
     cvv: joi.string().min(3).max(3).regex(/^[0-9]+$/).required(),
     expirationDate: joi.string().trim().regex(/^(0[1-9]|1[0-2])\/?([0-9]{2})$/).required(),
--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,12 +1,14 @@
 import { ISchemas } from "../interfaces/schemasInterface";
 import { authSchema } from "./authSchema";
+import { cardSchema } from "./cardSchema";
 import { credentialSchema } from "./credentialSchema";
 import { noteSchema } from "./noteSchema";
 
 const schemas: ISchemas = {
     "auth": authSchema,
     "credential": credentialSchema,
-    "note": noteSchema
+    "note": noteSchema,
+    "card": cardSchema
 };
 
 export { schemas };
