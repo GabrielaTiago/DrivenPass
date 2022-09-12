8bc843143fbc9e9cec408deb069d204bdb451b09
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-11 22:45:09 -0300

    :wrench: chore: Creates the card services

---
+++ src/services/cardsServices.ts
@@ -0,0 +1,13 @@
+
+
+export async function createCard() {
+}
+
+export async function getUserCards(userId: number) {
+}
+
+export async function getCardById(userId: number, cardId: number) {
+}
+
+export async function deleteCard(userId: number, cardId: number) {
+}
