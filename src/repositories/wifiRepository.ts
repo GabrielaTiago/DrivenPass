33b2c53f742030f5ef357330b6be11aae4b37ec9
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-12 07:58:54 -0300

    :wrench: chore: Creates the wifi repository

---
+++ src/repositories/wifiRepository.ts
@@ -0,0 +1,14 @@
+import { database } from "../config/postegres";
+
+
+export async function createWifi() {
+}
+
+export async function getUserWifis(userId: number) {
+}
+
+export async function getWifiById(userId: number, wifiId: number) {
+}
+
+export async function deleteWifi(wifiId: number) {
+}
