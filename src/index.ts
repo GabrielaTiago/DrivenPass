24a00088efb7723cb55a908b82de04a5898382bc
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-15 13:59:21 -0300

    :recycle: refactor: Update server port configuration and conditionally start server in non-test environments

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,12 @@ app.use(cors(), json());
 app.use(router);
 app.use(errorHandler);
 
-const PORT: number = Number(process.env.PORT) || 4001;
+const PORT: number = Number(process.env.PORT) || 4000;
 
-app.listen(PORT, () => {
-  console.log(`The server is up and runnig on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`The server is up and running on port ${PORT}`);
+  });
+}
+
+export { app };
