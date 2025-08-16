f9059acb3de75e43e1a95202582943877ef41e2d
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-15 21:16:42 -0300

    :sparkles: feat: Add createCredential method for database integration

--- src/tests/factories/credentialFactory.ts
+++ src/tests/factories/credentialFactory.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { Credential } from '@prisma/client';
 
+import { database } from '../../config/postgres';
 import { CredentialData } from '../../types/credentialType';
 
 export const credentialFactory = {
@@ -22,5 +23,18 @@ export const credentialFactory = {
       ...credentialData,
       ...override,
     };
+  },
+
+  async createCredential(override: Partial<Credential> = {}, userId?: number) {
+    return await database.credential.create({
+      data: {
+        title: faker.lorem.word(),
+        url: faker.internet.url(),
+        username: faker.internet.username(),
+        password: faker.internet.password(),
+        userId: userId || faker.number.int({ min: 1, max: 10000 }),
+        ...override,
+      },
+    });
   },
 };
