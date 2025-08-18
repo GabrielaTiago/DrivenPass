04db82824343ca71a20b98a9bd185b6d6f04c166
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-18 10:17:09 -0300

    :sparkles: feat: Add updateCredential function and improve response messages

--- src/controllers/credentialsController.ts
+++ src/controllers/credentialsController.ts
@@ -6,35 +6,36 @@ import { CredentialData } from '../types/credentialType';
 async function createCredential(req: Request, res: Response) {
   const userId = Number(res.locals.userId);
   const credential: CredentialData = req.body;
-
   await credentialsServices.createCredential(credential, userId);
-
-  res.status(201).send('Successfully created the credential');
+  res.status(201).send({ message: 'Credential created successfully' });
 }
 
 async function getUserCredentials(req: Request, res: Response) {
   const userId = Number(res.locals.userId);
-
   const userCredentials = await credentialsServices.getUserCredentials(userId);
-
   res.status(200).send(userCredentials);
 }
 
 async function getCredentialById(req: Request, res: Response) {
   const userId: number = Number(res.locals.userId);
   const credentialId: number = Number(req.params.id);
-
-  const specificCredential = await credentialsServices.getCredendtialById(userId, credentialId);
-
-  res.status(200).send(specificCredential);
+  const credential = await credentialsServices.getCredentialById(userId, credentialId);
+  res.status(200).send(credential);
 }
 
 async function deleteCredentials(req: Request, res: Response) {
   const userId: number = Number(res.locals.userId);
   const credentialId: number = Number(req.params.id);
-
   await credentialsServices.deleteCredential(userId, credentialId);
-  res.sendStatus(200);
+  res.status(200).send({ message: 'Credential deleted successfully' });
 }
 
-export { createCredential, deleteCredentials, getUserCredentials, getCredentialById };
+async function updateCredential(req: Request, res: Response) {
+  const userId = Number(res.locals.userId);
+  const credentialId = Number(req.params.id);
+  const credentialData: CredentialData = req.body;
+  await credentialsServices.updateCredential(userId, credentialId, credentialData);
+  res.status(200).send({ message: 'Credential updated successfully' });
+}
+
+export { createCredential, deleteCredentials, getUserCredentials, getCredentialById, updateCredential };
