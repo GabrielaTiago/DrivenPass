841dfff96023c238cb4cb4fbe904c21762368e21
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-18 16:31:57 -0300

    :sparkles: feat: Introduce cardFactory for generating mock card data and database entries

---
+++ src/tests/factories/cardFactory.ts
@@ -0,0 +1,42 @@
+import { faker } from '@faker-js/faker';
+import { Card } from '@prisma/client';
+
+import { database } from '../../config/postgres';
+import { CardData } from '../../types/cardType';
+
+export const cardFactory = {
+  createCardData(override: Partial<CardData> = {}): CardData {
+    return {
+      nickname: faker.lorem.word().slice(0, 20),
+      printedName: faker.person.fullName().toUpperCase().slice(0, 50),
+      number: faker.finance.creditCardNumber().slice(0, 16),
+      cvv: faker.finance.creditCardCVV().slice(0, 3),
+      expirationDate: `${faker.date.future().getMonth() + 1}`.padStart(2, '0') + '/' + faker.date.future().getFullYear().toString().slice(-2),
+      password: faker.number.int({ min: 1000, max: 9999 }).toString(),
+      type: faker.helpers.arrayElement(['credit', 'debit', 'both']),
+      virtual: faker.datatype.boolean(),
+      ...override,
+    };
+  },
+
+  createMockCard(override: Partial<Card> = {}, userId?: number) {
+    const cardData = this.createCardData();
+    return {
+      id: faker.number.int({ min: 1, max: 10000 }),
+      userId: userId || faker.number.int(),
+      createdAt: faker.date.recent(),
+      ...cardData,
+      ...override,
+    };
+  },
+
+  async createCard(override: Partial<Card> = {}, userId?: number) {
+    return await database.card.create({
+      data: {
+        userId: userId || faker.number.int({ min: 1, max: 10000 }),
+        ...this.createCardData(),
+        ...override,
+      },
+    });
+  },
+};
