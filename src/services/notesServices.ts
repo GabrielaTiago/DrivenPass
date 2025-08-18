1221da0daee643c27c7298fec197a5a570ec405a
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-18 10:34:04 -0300

    :recycle: refactor: Simplify and enhance note service functions

--- src/services/notesServices.ts
+++ src/services/notesServices.ts
@@ -2,44 +2,35 @@ import { throwErrorMessage } from '../middlewares/errorHandlerMiddleware';
 import * as notesRepository from '../repositories/notesRepository';
 import { NoteData } from '../types/noteType';
 
-export async function createNote(note: NoteData, userId: number) {
-  const moreThanOneTitle = await notesRepository.findMoreThanOneTitle(userId, note.title);
-
-  if (moreThanOneTitle) {
-    throw throwErrorMessage('conflict', 'You already have a credential with this title');
-  }
-
-  await notesRepository.createNote(note, userId);
+export async function createNote(data: NoteData, userId: number) {
+  const note = await notesRepository.findNoteTitleByUser(userId, data.title);
+  if (note) throw throwErrorMessage('conflict', 'Note with this title already exists');
+  await notesRepository.createNote(data, userId);
 }
 
 export async function getUserNotes(userId: number) {
-  return await notesRepository.getUserNotes(userId);
+  const notes = await notesRepository.getUserNotes(userId);
+  return notes;
 }
 
 export async function getNoteById(userId: number, noteId: number) {
-  const specificNote = await notesRepository.getNoteById(noteId);
-
-  if (!specificNote) {
-    throw throwErrorMessage('not_found', "It seems that this note doesn't exist yet");
-  }
-
-  if (specificNote.userId !== userId) {
-    throw throwErrorMessage('forbidden', "You don't have the permition to see this note");
-  }
-
-  return specificNote;
+  const note = await notesRepository.getNoteById(noteId);
+  if (!note) throw throwErrorMessage('not_found', "Note doesn't exist");
+  if (note.userId !== userId) throw throwErrorMessage('forbidden', "You don't have the permission to see this note");
+  return note;
 }
 
 export async function deleteNote(userId: number, noteId: number) {
-  const noteForDeletion = await notesRepository.getNoteById(noteId);
-
-  if (!noteForDeletion) {
-    throw throwErrorMessage('not_found', "It seems that this note doesn't exists");
-  }
-
-  if (noteForDeletion.userId !== userId) {
-    throw throwErrorMessage('forbidden', "You don't have the permition to delete this note");
-  }
-
+  const note = await notesRepository.getNoteById(noteId);
+  if (!note) throw throwErrorMessage('not_found', "Note doesn't exist");
+  if (note.userId !== userId) throw throwErrorMessage('forbidden', "You don't have the permission to delete this note");
   await notesRepository.deleteNote(noteId);
 }
+
+export async function updateNote(userId: number, noteId: number, data: NoteData) {
+  const note = await notesRepository.getNoteById(noteId);
+  if (!note) throw throwErrorMessage('not_found', "Note doesn't exist");
+  if (note.userId !== userId) throw throwErrorMessage('forbidden', "You don't have the permission to update this note");
+  await notesRepository.updateNote(noteId, data);
+}
+
