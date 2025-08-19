45ca18506b3df52c764355a3306a00368889dcf2
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-19 18:59:45 -0300

    :sparkles: feat: Integrate Swagger UI for API documentation

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { authRouter } from './authRouter';
 import { cardsRouter } from './cardsRouter';
 import { credentialRouter } from './credentialsRouter';
 import { noteRouter } from './notesRouter';
 import { wifiRouter } from './wifisRouter';
+import swaggerSpec from '../config/swagger';
 
 const router = Router();
 
+router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 router.use(authRouter);
 router.use(credentialRouter);
 router.use(noteRouter);
