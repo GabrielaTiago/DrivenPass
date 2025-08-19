912d99e6bd82fb8c1eef323ddf2eaab724bf63ba
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-19 01:42:06 -0300

    :recycle: refactor: Standardize response messages and streamline card controller functions

--- src/controllers/cardsController.ts
+++ src/controllers/cardsController.ts
@@ -3,38 +3,37 @@ import { Request, Response } from 'express';
 import * as cardsServices from '../services/cardsServices';
 import { CardData } from '../types/cardType';
 
-
 export async function createCard(req: Request, res: Response) {
   const userId = Number(res.locals.userId);
   const card: CardData = req.body;
-
   await cardsServices.createCard(card, userId);
-
-  res.status(201).send('Successfully created the card!');
+  res.status(201).send({ message: 'Card created successfully' });
 }
 
 export async function getUserCards(req: Request, res: Response) {
   const userId = Number(res.locals.userId);
-
-  const allUserCards = await cardsServices.getUserCards(userId);
-
-  res.status(200).send(allUserCards);
+  const userCards = await cardsServices.getUserCards(userId);
+  res.status(200).send(userCards);
 }
 
 export async function getCardById(req: Request, res: Response) {
-  const userId: number = Number(res.locals.userId);
-  const cardId: number = Number(req.params.id);
-
-  const specificCard = await cardsServices.getCardById(userId, cardId);
-
-  res.status(200).send(specificCard);
+  const userId = Number(res.locals.userId);
+  const cardId = Number(req.params.id);
+  const card = await cardsServices.getCardById(userId, cardId);
+  res.status(200).send(card);
 }
 
 export async function deleteCard(req: Request, res: Response) {
-  const userId: number = Number(res.locals.userId);
-  const cardId: number = Number(req.params.id);
-
+  const userId = Number(res.locals.userId);
+  const cardId = Number(req.params.id);
   await cardsServices.deleteCard(userId, cardId);
+  res.status(200).send({ message: 'Card deleted successfully' });
+}
 
-  res.status(200).send('Successfully delete the card!');
+export async function updateCard(req: Request, res: Response) {
+  const userId = Number(res.locals.userId);
+  const cardId = Number(req.params.id);
+  const cardData: CardData = req.body;
+  await cardsServices.updateCard(userId, cardId, cardData);
+  res.status(200).send({ message: 'Card updated successfully' });
 }
