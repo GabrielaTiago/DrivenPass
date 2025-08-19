d225901d68f824581d0965730ab39b2d3540bd07
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-19 01:42:44 -0300

    :sparkles: feat: Add updateCard endpoint to cardsRouter for card updates

--- src/routes/cardsRouter.ts
+++ src/routes/cardsRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { createCard, deleteCard, getCardById, getUserCards } from '../controllers/cardsController';
+import { createCard, deleteCard, getCardById, getUserCards, updateCard } from '../controllers/cardsController';
 import { validateSchema } from '../middlewares/validateSchemaMiddleware';
 import { validateToken } from '../middlewares/validateTokenMiddleware';
 import { schemas } from '../schemas/schemas';
@@ -10,6 +10,7 @@ const cardsRouter = Router();
 cardsRouter.post('/cards', validateSchema(schemas.card), validateToken, createCard);
 cardsRouter.get('/cards/all', validateToken, getUserCards);
 cardsRouter.get('/cards/:id', validateToken, getCardById);
-cardsRouter.delete('/cards/:id/delete', validateToken, deleteCard);
+cardsRouter.put('/cards/:id', validateSchema(schemas.card), validateToken, updateCard);
+cardsRouter.delete('/cards/:id', validateToken, deleteCard);
 
 export { cardsRouter };
